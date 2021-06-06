ab00fae72b8e8a2084cb265aabe3eb3377c12611
Nirushana <kavinda.n.t@gmail.com>
2021-06-06 23:44:27 +0530

    Feature added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Hero from './components/Hero';
-import Products from './components/Products/index';
-import { GlobalStyle } from './globalStyles';
-import { productData } from './components/Products/data'
- 
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Hero from "./components/Hero";
+import Products from "./components/Products/index";
+import { GlobalStyle } from "./globalStyles";
+import { productData } from "./components/Products/data";
+import Feature from "./components/Feature";
+
 function App() {
   return (
     <Router>
       <GlobalStyle />
-     <Hero />
-     <Products heading='Choose your favorite' data={productData}/>
+      <Hero />
+      <Products heading="Choose your favorite" data={productData} />
+      <Feature />
     </Router>
   );
 }
