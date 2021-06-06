172420cef678a4c0b0e800cbede5cc355be34550
Nirushana <kavinda.n.t@gmail.com>
2021-06-06 21:38:16 +0530

    product addition done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Hero from './components/Hero';
+import Products from './components/Products/index';
 import { GlobalStyle } from './globalStyles';
-
+import { productData } from './components/Products/data'
+ 
 function App() {
   return (
     <Router>
       <GlobalStyle />
      <Hero />
+     <Products heading='Choose your favorite' data={productData}/>
     </Router>
   );
 }
--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -3,13 +3,34 @@ import {
   ProductsContainter,
   ProductsHeading,
   ProductsWrapper,
+  ProductCard,
+  ProductImg,
+  ProductInfo,
+  ProductTitle,
+  ProductDesc,
+  ProductPrice,
+  ProductButton,
 } from "./ProductElements";
 
-function Products() {
+function Products({ heading, data }) {
   return (
     <ProductsContainter>
-      <ProductsHeading>Heading</ProductsHeading>
-      <ProductsWrapper></ProductsWrapper>
+      <ProductsHeading>{heading}</ProductsHeading>
+      <ProductsWrapper>
+        {data.map((product, index) => {
+          return (
+            <ProductCard key={index}>
+              <ProductImg src={product.img} alt={product.alt} />
+              <ProductInfo>
+                <ProductTitle>{product.name}</ProductTitle>
+                <ProductDesc>{product.desc}</ProductDesc>
+                <ProductPrice>{product.price}</ProductPrice>
+                <ProductButton>{product.button}</ProductButton>
+              </ProductInfo>
+            </ProductCard>
+          );
+        })}
+      </ProductsWrapper>
     </ProductsContainter>
   );
 }
