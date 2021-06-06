6d8e45103085f7865e07513b9c5d9d4e4d4469f2
Nirushana <kavinda.n.t@gmail.com>
2021-06-06 07:35:34 +0530

    Sidebar

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import Navbar from '../Navbar/index'
+import Sidebar from '../Sidebar'
 import { HeroContainter, HeroContent, HeroItems, HeroH1, HeroP, HeroBtn } from './HeroElements'
 
 function Hero() {
     return (
         <HeroContainter>
             <Navbar />
+            <Sidebar />
             <HeroContent>
                 <HeroItems>
                     <HeroH1>Greates Pizza Ever</HeroH1>
                     <HeroP>Ready in 60 seconds</HeroP>
-                    <HeroBtn>PlaceOrder</HeroBtn>
+                    <HeroBtn>Place Order</HeroBtn>
                 </HeroItems>
             </HeroContent>
         </HeroContainter>
